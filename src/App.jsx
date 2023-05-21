c48d26e35f2b11e20fbf2f1ced3e4f1a4ad23c2e
ankit puthran <30755686+puthranankit@users.noreply.github.com>
2023-05-21 03:05:38 +0100

    refactor: 💡 commented code in app.jsx (#1)
    commented code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
+        <p>This is an example of a Vite + React app running on Railway.</p>
       </div>
     </>
   )
